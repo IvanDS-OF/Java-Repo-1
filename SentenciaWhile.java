e72f2dddbe341e6c575d91682ecd4febb90740e5
IvanDS-OF <ivan.durans@outlook.com>
2024-06-10 11:18:50 -0600

    Add Sentence of Break and Continue in Java

--- SentenciaWhile.java
+++ SentenciaWhile.java
@@ -19,5 +19,29 @@ public class SentenciaWhile {
 
         System.out.println("Terminamos, Gracias. ");
 
+
+        //Vmoas a hacer uso del ciclo Do While
+        //La diferencia está en que es necesactio primero correr una vez la logica 
+        // ¿dentro de la sentencia y luego se hace la validacion de la sentencia. 
+        
+        // Vamos a solicitar al usuario escribir un numero, y el cliclo terminara hasta que sea 0
+
+        System.out.println("Por favor escribe un numero xd");
+
+        int RespuestaCero = scanner.nextInt();
+
+        int NumeroCero = 0;
+
+        do {
+            System.out.println("Estas dentro del bucle");
+            System.out.println("Tu numero no es valido para salir   ");
+            //Es necesario colocar una entrada aqui
+            RespuestaCero = scanner.nextInt();
+
+        } while (RespuestaCero != 0);
+        System.out.println("Has salido del bucle nene. ");
+
+
+
     }
 }
---
+++ SentenciasBreakContinue.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class SentenciasBreakContinue {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //Vamos a conocer ahora nuestras s entencias de control Break and Continue :D
+
+        //Break sirve para cortar una sentencia ciclica en un punto determinado dada una condicion determinada
+        //Vamos a iterar en For del 1 al 20 pero detenemos en 10
+        int ValorFinal = 5;
+        int Incremento = 0;
+        for (int i = 1; i <= 20 ; i++){
+            System.out.println("Estamos en el valor " + i);
+            Incremento = i;
+            //Para hacer uso correcto de la sentenci Break podemos hacer uso de una condicional If
+            if (Incremento == 10) {
+                System.out.println("Ya llevamos a 10 pa, fuga");
+                break;
+            }
+
+        }
+        System.out.println("Terminamos el clico en : " + Incremento + " Bai");
+
+        
+        
+        
+        
+        //Continue sirve para continuar una sentencia ciclica dad una condicion determinada en un punto determinado 
+
+        
+
+
+    }
+}
