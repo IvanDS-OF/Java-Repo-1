74a4b51a0ae6e32f313ffd541897c272e2e94023
IvanDS-OF <ivan.durans@outlook.com>
2024-06-11 08:32:49 -0600

    Enum Created

---
+++ Clases/EnumeracionClase.java
@@ -0,0 +1,8 @@
+public enum EnumeracionClase {
+    //ENUMERACIONES: Recordar que ahora no será tal cual una Class, sino que sera una Enum
+    //colocar  *public enum NameEnum(){}*
+    
+    
+
+
+}
