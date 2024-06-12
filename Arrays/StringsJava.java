3d85f25b0687aecba67bdc8a5105dbcbf1e2b22d
IvanDS-OF <ivan.durans@outlook.com>
2024-06-12 09:46:00 -0600

    Adding String file

---
+++ Arrays/StringsJava.java
@@ -0,0 +1,12 @@
+public class StringsJava {
+    public static void main(String[] args) {
+        //En todo lenguaje de programacion es importante saber como manipular Strings
+        //Para ello, Java tiene la facilidad de que los String son objetos
+        //Por lo que cuenta con las propiedades de ellos. 
+
+         
+
+
+
+    }
+}
