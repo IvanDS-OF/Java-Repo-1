199308283020eadc001c495d67beb3f5d1bd8a48
IvanDS-OF <ivan.durans@outlook.com>
2024-06-15 11:40:52 -0600

    Adding Encapsulamiento

---
+++ Arrays/cajeroAutomatico.java
@@ -0,0 +1,8 @@
+public class cajeroAutomatico {
+    public static void main(String[] args) {
+        
+
+        
+
+    }
+}
---
+++ Encapsulamiento/encapsulamiento.java
@@ -0,0 +1,17 @@
+public class encapsulamiento {
+    public static void main(String[] args) {
+        //El encapsulamiento sirve para blindar el contenido de nuestras clases 
+        //de otras externas a estas
+        //La unica forma para interactuar con nuestra clase creada tiene que se
+        //mediante los metodos publicos
+
+        //Esta seguridad se puede alcanar mediante diferentes niveles
+        //Con los modificadores de acceso 
+        //Con los paquetes - Son el modo que tiene Java para modificar las clases
+        //  agrupando las que estan relacionadas entre si
+        //Los Modulos - Son grupos de paquetes que incluyen la definicion de sus dependencias 
+
+
+
+    }
+}
