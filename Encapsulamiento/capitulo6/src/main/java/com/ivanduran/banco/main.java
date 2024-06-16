2ef3f3f72dfc0fdfd547952944b8d8d04bf0ab39
IvanDS-OF <ivan.durans@outlook.com>
2024-06-15 20:13:28 -0600

    Adding notes

---
+++ Encapsulamiento/capitulo6/src/main/java/com/ivanduran/banco/main.java
@@ -0,0 +1,9 @@
+package com.ivanduran.banco;
+
+public class main {
+    public static void main(String[] args) {
+        //Aqui es en donde vamos a hacer modificaciones y pruebas de la seguridad de nuestras clases
+        //así como nuestros paquetes
+    }
+
+}
