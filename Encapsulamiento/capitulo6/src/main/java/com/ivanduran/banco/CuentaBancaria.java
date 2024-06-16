c40b6c8785852d4a10e643605b34bec63cbaca76
IvanDS-OF <ivan.durans@outlook.com>
2024-06-15 20:57:16 -0600

    Add features

--- Encapsulamiento/capitulo6/src/main/java/com/ivanduran/banco/CuentaBancaria.java
+++ Encapsulamiento/capitulo6/src/main/java/com/ivanduran/banco/CuentaBancaria.java
@@ -4,40 +4,72 @@ public class CuentaBancaria {
 
     //Es aqui en donde creamos las propiedades de la clase cuenta Bancaria
 
-    //Nuestrs variables de instancia :D
+    //Nuestras variables de instancia :D
     private String titular;
     private TipoDeCuenta tipoDeCuenta;
     private double saldo;
 
 
+    //Para poder crear una instancia con esta clase en otra es necesario tener los 
+    //constructores como Publicos
+
     //Constructores :D
-    CuentaBancaria(String titular, TipoDeCuenta tipo, double saldo){
+    public CuentaBancaria(String titular, TipoDeCuenta tipo, double saldo){
         this.titular = titular;
         this.tipoDeCuenta = tipo;
         this.saldo = saldo;
     }
 
 
-    CuentaBancaria(String titular, double saldo){
+    public CuentaBancaria(String titular, double saldo){
         this(titular, TipoDeCuenta.AHORRO, saldo);
     }
 
-    CuentaBancaria(){
+    public CuentaBancaria(){
         this("", TipoDeCuenta.AHORRO, 0);
     }
 
     //METODOS :D
-    /* 
-    void sacarDinero(double cantidad){
-        return saldo - cantidad;
+    public void sacarDinero(double cantidad){
+        if(cantidad < 0){
+            return;
+            //Lo hacemos porque no podemos restar una cantidad negativa
+        }
+        double comision = calcularComision();
+        saldo = saldo - comision;
+        saldo = saldo - cantidad;
+        //No colocamos el Return porque solamente vamos a hacer modificaciones a las variables conocidas
     }
-    */
+
+    private double calcularComision(){ 
+        //Esste es un ejemplo de un metodo de uso interno
+        //No es necesario hacerlo publico
+        switch (tipoDeCuenta){
+            case AHORRO:
+                return COMISION;
+            case NOMINA:
+                return 0;
+            default:
+                return 0;
+        }
+    }
+
+    public void ingresarDinero(double cantidad){
+        if(cantidad < 0){
+            return;
+        }
+        saldo = saldo + cantidad;
+    }
 
 
+    protected void cambiarTipoDeCuenta(TipoDeCuenta nuevoTipo){
+        //Dado que este metodo no es muy usado, de momento lo colocaremos como Protected
+        tipoDeCuenta = nuevoTipo;
+    }
 
-
-
-
+    public double obtenerSaldo(){
+        return saldo;        
+    }
 
 
 
---
+++ Encapsulamiento/capitulo6/src/main/java/com/ivanduran/cajero/Main.java
@@ -0,0 +1,18 @@
+package com.ivanduran.cajero;
+
+import com.ivanduran.banco.CuentaBancaria;
+
+public class Main {
+    public static void main(String[] args) {
+        //Recordar que para poder crear una instancia en otra clase. es necesario tener Publicos los constructores
+        CuentaBancaria cuentaBancaria = new CuentaBancaria();
+
+        //Así como los metodos, es necesario hacerlos publicos para poder hacer uso de ellos
+        //Pero hay que tener cuidado y saber qué metodos si los podemos hacer publicos
+        
+
+
+
+
+    }
+}
