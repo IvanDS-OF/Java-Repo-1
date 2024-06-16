5db60b84e61e7dd80d9527f019ad9ead4e511ae3
IvanDS-OF <ivan.durans@outlook.com>
2024-06-15 20:02:04 -0600

    Finishing my Maven project structure

---
+++ Encapsulamiento/capitulo6/src/main/java/com/ivanduran/banco/tipoDeCuenta.java
@@ -0,0 +1,10 @@
+package com.ivanduran.banco;
+
+public enum tipoDeCuenta {
+    //Recordamos que las enumeraciones son simplemente para que poddamos
+    //definir el valor que pueden tener algunas de nuestras variables
+    AHORRO, NOMINA, HIPOTECA, INFANTIL;
+
+
+
+}
