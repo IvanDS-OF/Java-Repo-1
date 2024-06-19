17de01ad270b3ff9fc1c4eace81617d7fbf5598b
IvanDS-OF <ivan.durans@outlook.com>
2024-06-19 12:43:12 -0600

    Writing the basis of my code :D

---
+++ Java - Basic Structures/PracticasIniciacion/Ejemplo01.java
@@ -0,0 +1,10 @@
+package PracticasIniciacion;
+
+public class Ejemplo01 {
+    public static void main(String[] args) {
+        //Las clases main siempre seran void, nunca devolveran nada. en otro caso devolverán un error
+        System.out.println("Estoy aprendiendo a programar bien en Java :D");
+        
+
+    }
+}
