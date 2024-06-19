360f9f9872c693dd2232ac08e0509470b98521f1
IvanDS-OF <ivan.durans@outlook.com>
2024-06-19 15:46:02 -0600

    Little modifications

--- Java - Basic Structures/PracticasIniciacion/Ejemplo01.java
+++ Java - Basic Structures/PracticasIniciacion/Ejemplo01.java
@@ -4,6 +4,8 @@ public class Ejemplo01 {
     public static void main(String[] args) {
         //Las clases main siempre seran void, nunca devolveran nada. en otro caso devolverán un error
         System.out.println("Estoy aprendiendo a programar bien en Java :D");
+        //From here, all my codes will be described in english 
+
         
 
     }
