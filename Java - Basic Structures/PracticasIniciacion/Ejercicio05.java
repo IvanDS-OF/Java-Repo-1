477af439f4c7fe4e52ae0ede38420d97a2c6bab1
IvanDS-OF <ivan.durans@outlook.com>
2024-06-19 22:56:59 -0600

    Using If sentence

---
+++ Java - Basic Structures/PracticasIniciacion/Ejercicio05.java
@@ -0,0 +1,22 @@
+package PracticasIniciacion;
+
+public class Ejercicio05 {
+    public static void main(String[] args) {
+        //In this program, we will use the If sentences
+        //We need to count the number of arguments written
+
+        int  numeroArg = args.length;
+
+        //Use the conditionel
+        if (numeroArg > 0) {
+            System.out.println("There are " + numeroArg + " arguments");
+        } else {
+            System.out.println("There are no arguments");
+        }
+
+
+
+
+
+    }
+}
