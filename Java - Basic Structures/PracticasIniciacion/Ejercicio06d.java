f6bd1a8e391ba57a2fcdf021e94a663c883043cc
IvanDS-OF <ivan.durans@outlook.com>
2024-06-20 23:08:33 -0600

    Using While statement

---
+++ Java - Basic Structures/PracticasIniciacion/Ejercicio06d.java
@@ -0,0 +1,42 @@
+package PracticasIniciacion;
+
+public class Ejercicio06d {
+    private static final int LIM = 10;
+
+    public static void main(String[] args) {
+        //Exercise: Show the first argument that will be a long word (more than 10 characters)
+        //The x word is huge, 
+        //if not, show "All is correct, bye"
+
+        int i = 0;
+        
+        while (i < args.length) {
+            
+            String s = args[i];
+
+            if (s.length() > LIM) {
+                System.out.println("The word " + args[i] + "is very huge :D");
+                break;
+            }
+
+            if (i == args.length -1 ) {
+                System.out.println("All is ok, bye");
+                break;
+            }
+
+            i++;
+
+            System.out.println("This is my current position: " + i);
+
+        }
+
+
+
+
+
+
+
+
+    }
+
+}
--- Java - Basic Structures/PracticasIniciacion/Factorial.java
+++ Java - Basic Structures/PracticasIniciacion/Factorial.java
@@ -7,14 +7,19 @@ public class Factorial {
         int number = Integer.valueOf(args[0]);
         System.out.println(number);
         int res = 1;
-        int i = 0; //Starting the increment
+        int i = number; //Starting the increment
+        boolean condition = i < 0;
+
+        System.out.println("Initializating my i: " + i);
+        System.out.println("Result initial value: " + res);
+        System.out.println("Condition of my While sentence: " + condition);
         
-        while (i < 0) {
+        while (i > 0) {
             //While I is higer than 0, do the actions inside
-            res = res * number;
+            res = res * i;
             i = i - 1;
-            System.out.println(res);
-            System.out.println(i);
+            //System.out.println(res);
+            //System.out.println(i);
         }
 
         System.out.println("The result is: " + res);
