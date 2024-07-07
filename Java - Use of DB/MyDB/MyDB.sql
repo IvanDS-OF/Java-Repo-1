9e96b403f581a602c8bc6a4874650cac8bdc1185
IvanDS-OF <ivan.durans@outlook.com>
2024-07-07 12:05:26 -0600

    Adding information to my notes

---
+++ Java - Use of DB/MyDB/MyDB.sql
@@ -0,0 +1,6 @@
+SELECT * FROM otrareunionmas.reunion;
+
+INSERT INTO `otrareunionmas`.`reunion` (`fecha`, `asunto`) VALUES ('2024-02-01 13:30', 'La segunda reunion');
+
+SELECT * FROM otrareunionmas.reunion;
+
